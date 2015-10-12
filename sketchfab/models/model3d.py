3d49b2e6a12c5235dc32070085c2faa613e2ba9d
LaurentGrima <grima.laurent@gmail.com>
2015-10-13 00:38:27 +0200

    Add default=0 to views attribute in Model3D model.

--- sketchfab/models/model3d.py
+++ sketchfab/models/model3d.py
@@ -11,7 +11,7 @@ class Model3D(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     date_created = models.DateField(default=timezone.now)
-    views = models.IntegerField()
+    views = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
